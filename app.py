dc2355656a287a7ae4a9eb1ee0197dc6c951f092
Aaron Thalakkottor Sooraj <126183772+ATS-001@users.noreply.github.com>
2026-07-05 00:13:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import TextLoader
-from langchain.text_splitter import CharacterTextSplitter
+from langchain_text_splitters import CharacterTextSplitter
 from langchain.chains import RetrievalQA
 from langchain_groq import ChatGroq
 from pytube import YouTube
